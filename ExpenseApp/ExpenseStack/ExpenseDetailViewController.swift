4bdb74c0ff0b19e8f8fd6ffd247f90443c5982d7
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-06-19 15:44:33 +0200

    operation

---
+++ ExpenseApp/ExpenseStack/ExpenseDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExpenseDetailViewController.swift
+//  ExpenseApp
+//
+//  Created by mac on 19.06.2023.
+//
+
+import UIKit
+
+class ExpenseDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
