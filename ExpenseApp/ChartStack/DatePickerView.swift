f3a187b902de0ddc40c62c759833dd3458c6cb14
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-06-27 01:10:21 +0200

    datePickerView...

--- ExpenseApp/ChartStack/DatePickerView.swift
+++ ExpenseApp/ChartStack/DatePickerView.swift
@@ -7,17 +7,58 @@
 
 import UIKit
 
-class DatePickerView: UIView {
-    
-    
-    
-    
-    
-}
-
 enum DatePickerPeriod {
     case week
     case month
     case quarter
     case year
 }
+
+class DatePickerView: UIView {
+    
+    private lazy var fromDateTextField: UITextField = {
+        let textField = UITextField()
+        textField.isHidden = true
+        return textField
+    }()
+    
+    private lazy var tillTextField: UITextField = {
+        let textField = UITextField()
+        textField.isHidden = true
+        return textField
+    }()
+    
+    private lazy var periodTextField: UITextField = {
+        let textField = UITextField()
+        textField.isHidden = true
+        return textField
+    }()
+    
+   private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        return datePicker
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+
