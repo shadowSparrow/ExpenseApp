7654a229583f8e8762f7a8a936bc69b07dddd256
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-02 22:40:49 +0200

    keyboardNotification

--- ExpenseApp/ExpenseStack/ExpenseViewController.swift
+++ ExpenseApp/ExpenseStack/ExpenseViewController.swift
@@ -117,7 +117,7 @@ class ExpenseViewController: UIViewController {
                }
         }
     
-    private func setKeyboardNotification(){
+    private func setKeyboardNotification() {
             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         }
